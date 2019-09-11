85312e1d77843c2c096f6cb57cce50693e9aa3e7
telows <lolryichylol@hotmail.com>
2019-09-10 22:24:03 -0700

    added image hashing

--- finder.py
+++ finder.py
@@ -13,23 +13,23 @@ import imagehash
 
 
 
+#create class for hashes and associated img name
+class hashed_img:
+
+	def __init__(self, name, hash):
+		self.hash = hash
+		self.name = name
+		self.path = ""
+
+
 #add all duplicates (ones that occour after) to a list
 #delete all in the list if opted into 
 
 
-files = os.listdir()
-
-print(len(files))
-
 
 
 def hash(path):
-	with open(path, 'rb') as f:
-		return f.read() #hash stuff
-		#hash = imagehash.average_hash(Image.open('test.png'))
-
-
-
+	print(1)
 
 
 #open folder
@@ -37,6 +37,37 @@ def hash(path):
 #get all files in path
 #list strings with path + names
 def gen_list(path):
+	
+	files = os.listdir(path) #gives list of file names
+	imgs = []
+
+	#filter out webms and maybe gifs?
+	for i in files:
+		if ".webm" in i:
+			files.remove(i)
+
+	#hash images, and create hashed image objs
+	for i in files:
+
+		hash = imagehash.dhash(Image.open(path + i))
+		h_im = hashed_img(i, hash)
+		imgs.append(h_im)
+
+	return imgs
 
 
 
+
+def compare(ims):
+
+	t = 1
+
+
+#path for test
+path = 'C:\\Users\\XPS\\Pictures\\test\\'
+
+files = gen_list(path)
+
+print(len(files))
+
+compare(files)
